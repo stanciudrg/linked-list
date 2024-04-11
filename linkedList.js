8f4fca84516f852227d9e1f4728e507d6fe786b6
stanciudrg <stanciudragos1337@gmail.com>
2024-04-11 16:27:43 +0300

    Create prepend method on linkedList

--- linkedList.js
+++ linkedList.js
@@ -22,4 +22,24 @@ class LinkedList {
 
     tmp.next = new Node(data);
   }
+
+   // Adds a new node containing 'data' to the start of the list
+   prepend(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  // Returns the total number of nodes in the list
+  size() {
+    if (this.head === null) return 0;
+
+    let tmp = this.head;
+    let size = 1;
+
+    while (tmp.next !== null) {
+      tmp = tmp.next;
+      size++;
+    }
+
+    return size;
+  }
 }
