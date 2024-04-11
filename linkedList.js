8d22ec1f3d6deafa58410f47812cb9e4e87d9e6f
stanciudrg <stanciudragos1337@gmail.com>
2024-04-11 16:27:23 +0300

    Create append method on LinkedList

--- linkedList.js
+++ linkedList.js
@@ -9,4 +9,17 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  // Adds a new node containing 'data' to the end of the list
+  append(data) {
+    if (this.head === null) return (this.head = new Node(data));
+
+    let tmp = this.head;
+
+    while (tmp.next !== null) {
+      tmp = tmp.next;
+    }
+
+    tmp.next = new Node(data);
+  }
 }
