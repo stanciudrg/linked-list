256517e072134c0450c2750f0ecbde725366d88b
stanciudrg <stanciudragos1337@gmail.com>
2024-04-16 11:37:32 +0300

    Create get() and toArray() methods

--- linkedList.js
+++ linkedList.js
@@ -50,8 +50,8 @@ export default class LinkedList {
 
   // Returns the last node in the list
   getTail() {
-    // If the list is empty, return the head (null);
-    if (this.head === null) return this.head;
+    // If the list is empty, there is no tail - return null;
+    if (this.head === null) return null;
 
     let tmp = this.head;
 
@@ -62,8 +62,31 @@ export default class LinkedList {
     return tmp;
   }
 
+  // Returns the node containing 'data', otherwise returns null
+  get(data) {
+    // If the list is empty, there is nothing to get - return null;
+    if (this.head === null) return null;
+    // If head has 'data', return the head
+    if (this.head.data === data) return this.head;
+
+    let tmp = this.head;
+
+    while (tmp !== null) {
+      if (tmp.data === data) return tmp;
+      tmp = tmp.next;
+    }
+
+    return null;
+  }
+
   // Returns the node at the given index, otherwise returns undefined
   at(index) {
+    // Ignore requests for indexes lower than 0;
+    if (index < 0) return;
+    // If the list is empty, there is nothing to return - stop;
+    if (this.head === null) return;
+    if (index === 0) return this.head;
+
     let tmp = this.head;
     let counter = 0;
 
@@ -78,7 +101,7 @@ export default class LinkedList {
 
   // Removes the last element from the list
   pop() {
-    // If the list is empty, return undefined
+    // If the list is empty, there is nothing to remove - return
     if (this.head === null) return;
 
     let tmp = this.head;
@@ -103,6 +126,8 @@ export default class LinkedList {
 
   // Returns true if the passed in data is in the list, otherwise returns false
   contains(data) {
+    // If the list is empty, there is nothing to find - return false
+    if (this.head === null) return false;
     let tmp = this.head;
 
     while (tmp !== null) {
@@ -115,6 +140,11 @@ export default class LinkedList {
 
   // Returns the index of the node containing the data, otherwise returns null
   find(data) {
+    // If the list is empty, there is nothing to find - return null
+    if (this.head === null) return null;
+    // If the head contains 'data', return its index (0);
+    if (this.head.data === data) return 0;
+
     let tmp = this.head;
     // Index === number of iterations within the while loop
     let index = 0;
@@ -130,6 +160,9 @@ export default class LinkedList {
 
   // Represents the linkedList nodes as strings
   toString() {
+    // If the list is empty, there is nothing to transform into a string - return
+    if (this.head === null) return;
+
     let tmp = this.head;
     let string = "";
 
@@ -145,6 +178,22 @@ export default class LinkedList {
     return string;
   }
 
+  // Returns an array containing the data of each node
+  toArray() {
+    // If the list is empty, there is nothing to push into the array - return empty array
+    if (this.head === null) return [];
+
+    let tmp = this.head;
+    let array = [];
+
+    while (tmp !== null) {
+      array.push(tmp.data);
+      tmp = tmp.next;
+    }
+
+    return array;
+  }
+
   // Inserts a new node containing the provided data at the given index
   insertAt(data, index) {
     // Ignore requests for indexes lower than 0;
@@ -152,6 +201,8 @@ export default class LinkedList {
     // Fall back on the prepend method when the request is to add the node at
     // the beginning of the list
     if (index === 0) return this.prepend(data);
+    // If list is empty and the request is not to prepend, stop
+    if (index > 0 && this.head === null) return false;
 
     // Keep track of previous and current nodes
     let tmp = this.head;
@@ -163,7 +214,7 @@ export default class LinkedList {
     while (counter < index) {
       // Break the loop and prevent further action if the end of the list is
       // reached before the counter reaches the index (the index does not exist);
-      if (tmp === null) return;
+      if (tmp === null) return false;
       previous = tmp;
       tmp = tmp.next;
       counter++;
@@ -172,10 +223,13 @@ export default class LinkedList {
     // Set the previous node's next value to a new Node that contains the provided data,
     // then set the new node's next value to the current node (tmp);
     previous.next = new Node(data, tmp);
+    return true;
   }
 
   // Removes the node at the given index
   removeAt(index) {
+    // If the list is empty, there is nothing to remove - stop
+    if (this.head === null) return false;
     // Ignore requests for indexes lower than 0;
     if (index < 0) return;
     // Replace the current head node with the head.next node
@@ -191,7 +245,7 @@ export default class LinkedList {
     while (counter < index) {
       // Break the loop and prevent further action if the end of the list is
       // reached before the counter reaches the index (the index does not exist);
-      if (tmp.next === null) return;
+      if (tmp.next === null) return false;
       previous = tmp;
       tmp = tmp.next;
       counter++;
@@ -202,14 +256,18 @@ export default class LinkedList {
     // linked list after: previous.next => tmp.next
     // The middle node (tmp) has been removed
     previous.next = tmp.next;
+    return true;
   }
 
   // Loops through the list until 'data' is found, then removes it and returns true,
   // otherwise returns false
   findAndRemove(data) {
+    // If the list is empty, there is nothing to remove - stop
+    if (this.head === null) return false;
     // Replace the current head node with the head.next node
     // when the request is to remove the first node within the list
     if (this.head.data === data) return (this.head = this.head.next);
+
     // Keep track of previous and current nodes
     let tmp = this.head;
     let previous = tmp;
